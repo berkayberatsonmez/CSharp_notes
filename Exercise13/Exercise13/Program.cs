231d3c649c9da0571b18b84d2e712acd11f50f8e
BBS <60455005+berkayberatsonmez@users.noreply.github.com>
2023-03-27 19:54:18 +0300

    Add files via upload

---
+++ Exercise13/Exercise13/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercise13
+{
+    partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the width of the image");
+                int width = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the height of the image");
+                int height = int.Parse(Console.ReadLine());
+
+                if (width > height)
+                {
+                    Console.WriteLine("The image is in landscape orientation.");
+                }
+                else if (height > width)
+                {
+                    Console.WriteLine("The image is in portrait orientation.");
+                }
+                else
+                {
+                    Console.WriteLine("The image is square.");
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
