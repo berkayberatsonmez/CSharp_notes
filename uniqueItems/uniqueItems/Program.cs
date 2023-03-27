b2299084c4ed23173b097eeef46d1a414fa603c2
BBS <60455005+berkayberatsonmez@users.noreply.github.com>
2023-03-27 20:02:45 +0300

    Add files via upload

---
+++ uniqueItems/uniqueItems/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace uniqueItems
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        { 
+            var numbersList = new List<int>();
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Enter a number or quit for enter quit");
+                    var input = Console.ReadLine();
+                    if (input.ToLower() == "quit")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        var number = Convert.ToInt32(input);
+                        numbersList.Add(number);
+                    }
+                }
+                var newlist = numbersList.Distinct();
+                foreach (var number in newlist)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            catch 
+            {
+                Console.WriteLine("Enter just number");
+            }
+            
+            
+
+        }
+    }
+}
---
+++ untilOk/untilOk/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace untilOk
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int sum = 0;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter a number");
+                    string value = Console.ReadLine();
+                    if (value.ToLower() == "ok")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                       sum += Convert.ToInt32(value);
+                    }
+                }
+                catch 
+                {
+                    Console.WriteLine("Enter just number");
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
