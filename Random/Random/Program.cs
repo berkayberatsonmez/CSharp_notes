02a3e5c58c4fabdd9907ff1f860468c148670ffc
BBS <60455005+berkayberatsonmez@users.noreply.github.com>
2023-03-27 20:01:28 +0300

    Add files via upload

---
+++ Random/Random/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace RandommPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var random = new Random();
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength];
+            
+
+            for (var i = 0; i < passwordLength; i++)
+            {
+                buffer[i] = (char)('a' + random.Next(0,26)); 
+            }
+            var password = new string(buffer);
+            Console.WriteLine(password);
+            //Console.WriteLine((int)'a');
+        }
+    }
+}
---
+++ ReverseName/ReverseName/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ReverseName
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var nameList = new List<string>();
+            Console.WriteLine("Enter a name");
+            string name = Console.ReadLine();
+            try
+            {
+                foreach (var i in name)
+                {
+                    nameList.Add(i.ToString());
+                }
+                nameList.Reverse();
+                string result = string.Join("", nameList);
+                Console.WriteLine(result);
+            }
+            catch 
+            {
+                Console.WriteLine("Enter just a name!");
+            }
+
+        }
+    }
+}
