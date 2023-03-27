0413ac457d165f16d021036002b27053db46e79a
BBS <60455005+berkayberatsonmez@users.noreply.github.com>
2023-03-27 19:57:53 +0300

    Add files via upload

---
+++ facebookMessage/facebookMessage/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace facebookMessage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var listOfName = new List<string>();
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Enter a name (or press Enter to quit):  ");
+                    var input = Console.ReadLine();
+                    if (String.IsNullOrWhiteSpace(input))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        listOfName.Add(input);
+                    }
+                }
+                if (listOfName.Count == 0)
+                {
+                    Console.WriteLine(" ");
+                }
+                else if (listOfName.Count == 1)
+                {
+                    Console.WriteLine("{0} likes your post.", listOfName[0]);
+                }
+                else if (listOfName.Count == 2)
+                {
+                    Console.WriteLine("{0} and {1} like your post.", listOfName[0], listOfName[1]);
+                }
+                else 
+                {
+                    Console.WriteLine("{0}, {1}, and +{2} others like your post.", listOfName[0], listOfName[1], (listOfName.Count - 2));
+                }
+                
+            }
+            catch 
+            {
+                Console.WriteLine("Enter just name");
+            }
+        }
+    }
+}
---
+++ factorial/factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace factorial
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number which you want to factorial");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int fact = 1;
+            for (var i = 1; i <= number; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine(fact);
+
+        }
+    }
+}
