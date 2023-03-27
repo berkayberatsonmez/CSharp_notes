0caad829afca9d1cf4a980d7c41ff4acd447e099
BBS <60455005+berkayberatsonmez@users.noreply.github.com>
2023-03-27 19:57:01 +0300

    Add files via upload

---
+++ Exercises1/Exercises1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercises1
+{
+    partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number between 1 to 10: ");
+
+            try
+            {
+                string numberString = Console.ReadLine();
+                int number = int.Parse(numberString);
+                if (10 > number && number > 1)
+                {
+                    Console.WriteLine("Valid");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Enter just numbers");
+            }
+        }
+    }
+}
