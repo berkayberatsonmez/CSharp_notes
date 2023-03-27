0580e5fc9a12e7dc2847d9e831a9c49e6428c9c3
BBS <60455005+berkayberatsonmez@users.noreply.github.com>
2023-03-27 20:00:14 +0300

    Add files via upload

---
+++ maxOfArray/maxOfArray/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace maxOfArray
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try 
+            {
+                Console.WriteLine("enter a series of numbers separated by comma:");
+                var input = Console.ReadLine();
+                string[] numbers = input.Split(',');
+                int maxNumber = int.Parse(numbers[0]);
+                for (var i = 1;  i < numbers.Length; i++)
+                {
+                    if (maxNumber < int.Parse(numbers[i]))
+                    {
+                        maxNumber = int.Parse(numbers[i]);
+                    }
+                }
+                Console.WriteLine(maxNumber);
+
+            }
+            catch 
+            {
+                Console.WriteLine("Enter just number or spearate by comma.");
+            }
+
+
+        }
+    }
+}
---
+++ onetoHundered/onetoHundered/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace onetoHundered
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0) 
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
