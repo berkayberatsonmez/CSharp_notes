90a0480cfb13da7029cfcb5929b04b49773c45da
BBS <60455005+berkayberatsonmez@users.noreply.github.com>
2023-03-27 19:53:32 +0300

    Add files via upload

---
+++ Exercise1.2/Exercise1.2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercise12
+{
+    partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter first number");
+                string numberOne = Console.ReadLine();
+                Console.WriteLine("Enter second number");
+                string numberTwo = Console.ReadLine();
+                int one = Convert.ToInt16(numberOne);
+                int two = Convert.ToInt16(numberTwo);
+                if (one > two)
+                {
+                    Console.WriteLine(one);
+                }
+                else
+                {
+                    Console.WriteLine(two);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Enter just number");
+            }
+            }
+            
+        }
+}
+
